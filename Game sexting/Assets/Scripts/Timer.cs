2931c5a29edc036fd8d58d7a3b26d660af38a600
Joppe Demulder <joppe.demulder@hotmail.com>
2018-04-16 15:47:11 +0200

    timer

---
+++ Game sexting/Assets/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public School[] schools = new School[2];
+    public ushort totalStudentsReached;
+    public Text counterText;
+
+	void Update () {
+        totalStudentsReached = 0;
+
+		for(int i = 0; i < schools.Length; i++)
+        {
+            totalStudentsReached += schools[i].nbrOfReachedStudents;
+        }
+
+        UpdateUI();
+	}
+
+    void UpdateUI()
+    {
+        counterText.text = totalStudentsReached.ToString();
+    }
+}
