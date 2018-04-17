7fb8b008126a013b0997b0ef7a5b053fb953bc52
Joppe Demulder <joppe.demulder@hotmail.com>
2018-04-17 18:31:57 +0200

    Change timer to counter

--- Game sexting/Assets/Scripts/Counter.cs
+++ Game sexting/Assets/Scripts/Counter.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Timer : MonoBehaviour {
+public class Counter : MonoBehaviour
+{
 
     public School[] schools = new School[2];
     public ushort totalStudentsReached;
     public Text counterText;
 
-	void Update () {
+    void Update()
+    {
         totalStudentsReached = 0;
 
-		for(int i = 0; i < schools.Length; i++)
+        for (int i = 0; i < schools.Length; i++)
         {
             totalStudentsReached += schools[i].nbrOfReachedStudents;
         }
 
         UpdateUI();
-	}
+    }
 
     void UpdateUI()
     {
