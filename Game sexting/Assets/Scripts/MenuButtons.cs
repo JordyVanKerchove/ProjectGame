840fbd9180a6d614f817d3e1e0b525f44031125f
JordyVanKerchove <jordy.vankerchove@student.kdg.be>
2018-04-26 15:13:46 +0200

    start & options menu

---
+++ Game sexting/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour {
+
+    public GameObject Startmenu;
+    public GameObject OptionsMenu;
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Opties()
+    {
+        Startmenu.SetActive(false);
+        OptionsMenu.SetActive(true);
+    }
+
+    public void Vorige()
+    {
+        Startmenu.SetActive(true);
+        OptionsMenu.SetActive(false);
+    }
+
+    
+}
