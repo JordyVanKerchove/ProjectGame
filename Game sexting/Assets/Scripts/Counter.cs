1221d1f8980040bd8e6bc06ba1bbf39c2640dacd
Joppe Demulder <joppe.demulder@hotmail.com>
2018-05-17 10:59:36 +0200

    NbrOfStudents to %

--- Game sexting/Assets/Scripts/Counter.cs
+++ Game sexting/Assets/Scripts/Counter.cs
@@ -2,34 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class Counter : MonoBehaviour {
 
     public School[] schools = new School[5];
     public Text UICounter;
     public Text[] UIMiniCounters = new Text[5];
-    public ushort totalNbrOfInfectedStudents;
+    public ushort totalNbrOfReachedStudents;
+    public ushort totalNbrOfStudents;
+    public float totalPercent;
+    float tempPercent;
 
     // Use this for initialization
-    void Start () {
-        
-	}
+    void Start ()
+    {
+        totalPercent = 0;
+        totalNbrOfStudents = 0;
+
+        for (int i = 0; i < schools.Length; i++)
+        {
+            totalNbrOfStudents += schools[i].nbrOfStudents;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        totalNbrOfInfectedStudents = 0;
+        totalNbrOfReachedStudents = 0;
 
 		for(int i = 0; i < schools.Length; i++)
         {
-            totalNbrOfInfectedStudents += schools[i].nbrOfReachedStudents;
-            UIMiniCounters[i].text = schools[i].nbrOfReachedStudents.ToString();
+            tempPercent = 0;
+            totalNbrOfReachedStudents += schools[i].nbrOfReachedStudents;
+            tempPercent = schools[i].nbrOfReachedStudents * 100;
+            tempPercent /= schools[i].nbrOfStudents;
+            tempPercent = (float)Math.Round(tempPercent);
+            UIMiniCounters[i].text = tempPercent + "%";
         }
+        totalPercent = totalNbrOfReachedStudents * 100;
+        totalPercent /= totalNbrOfStudents;
+        totalPercent = (float)Math.Round(totalPercent);
 
         UpdateUI();
 	}
     
     void UpdateUI()
     {
-        UICounter.text = totalNbrOfInfectedStudents.ToString();
+        UICounter.text = totalPercent.ToString() + "%";
     }
 }
